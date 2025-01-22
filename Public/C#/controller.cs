f98f1009d0acb502dd2effcdc45bcfdf6465ccc0
BBaykhan <bobur.baykhanov@gmail.com>
2025-01-23 00:10:21 +0900

    movement 2D

--- Public/C#/controller.cs
+++ Public/C#/controller.cs
@@ -2,18 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Controller : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
+
     private bool isMoving;
+
     private Vector2 input;
+    
     private void Update()
     {
         if (!isMoving)
         {
-            input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+
             if (input != Vector2.zero)
             {
+
+                if (input.x != 0) input.y = 0;
+
                 var targetPos = transform.position;
                 targetPos.x += input.x;
                 targetPos.y += input.y;
@@ -21,20 +29,18 @@ public class Controller : MonoBehaviour
                 StartCoroutine(Move(targetPos));
             }
         }
-    }   
-}
-
-
-IEnumerator Move(Vector3 targetPos)
-{
-    isMoving = true;
-
-    while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
-    {
-        transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
-        yield return null;
     }
-    transform.position = targetPos;
 
-    isMoving = false;
+    IEnumerator Move(Vector3 targetPos)
+    {
+        isMoving = true;
+        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = targetPos;
+
+        isMoving = false;
+    }
 }
