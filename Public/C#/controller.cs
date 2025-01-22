350cb9cda368901d6a8dba39d6d9f0431cdd6e66
BBaykhan <bobur.baykhanov@gmail.com>
2025-01-22 23:55:04 +0900

    movement 2D

--- Public/C#/controller.cs
+++ Public/C#/controller.cs
@@ -14,8 +14,27 @@ public class Controller : MonoBehaviour
             input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
             if (input != Vector2.zero)
             {
-                StartCoroutine(Move(transform));
+                var targetPos = transform.position;
+                targetPos.x += input.x;
+                targetPos.y += input.y;
+
+                StartCoroutine(Move(targetPos));
             }
         }
+    }   
+}
+
+
+IEnumerator Move(Vector3 targetPos)
+{
+    isMoving = true;
+
+    while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+        yield return null;
     }
+    transform.position = targetPos;
+
+    isMoving = false;
 }
